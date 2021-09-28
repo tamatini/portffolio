7b091615bb876ab10ff404afce73b607ef7ef51f
Tamatini <tamatini@hotmail.fr>
2021-09-27 17:33:04 -1000

    https redirection

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import HttpsRedirect from 'react-https-redirect'
 
 AOS.init();
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HttpsRedirect>
+      <App />
+    </HttpsRedirect>
   </React.StrictMode>,
   document.getElementById('root')
 );
